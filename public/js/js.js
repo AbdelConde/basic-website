46dfa2ddc4951d82a483de94044ad16b801a2d43
d2gomez <44311491+d2gomez@users.noreply.github.com>
2018-10-20 02:01:48 -0600

    Update js.js

--- public/js/js.js
+++ public/js/js.js
@@ -1,11 +1,13 @@
 function misAudios()
 {
+        $("#control_audio").removeClass("hide");
 	document.getElementById("control_audio").style.display='';
         document.getElementById("my_audio").style.display='';
 	document.getElementById("contenido").innerHTML='<div class="widget"><h1>Stored Recordings</h1><ul id="recordingslist"></ul></div>';
 }
 function perfil()
 {
+    $("#control_audio").addClass("hide");
 	document.getElementById("control_audio").style.display='none';
         document.getElementById("my_audio").style.display='none';
 	document.getElementById("contenido").innerHTML='<div class="widget"></div>';
